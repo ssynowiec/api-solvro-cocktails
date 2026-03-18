76490c27ec3683b3a558c6312fa87274f4edd612
Stanisław Synowiec <stasyn1410@gmail.com>
2026-03-18 19:44:22 +0100

    chore(schema): export Cocktail Dto

--- src/db/schema/cocktails.ts
+++ src/db/schema/cocktails.ts
@@ -1,6 +1,20 @@
 import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema,
+} from "drizzle-zod";
+import type { z } from "zod";
 
 export const cocktails = pgTable("cocktails", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: text().notNull(),
 });
+
+export const cocktailSelectSchema = createSelectSchema(cocktails);
+export const cocktailInsertSchema = createInsertSchema(cocktails);
+export const cocktailUpdateSchema = createUpdateSchema(cocktails);
+
+export type CocktailDto = z.infer<typeof cocktailSelectSchema>;
+export type CreateCocktailDto = z.infer<typeof cocktailInsertSchema>;
+export type UpdateCocktailDto = z.infer<typeof cocktailUpdateSchema>;
