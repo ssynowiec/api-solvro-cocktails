c031a061a0ac50854713baa25e026325a14e2611
Stanisław Synowiec <stasyn1410@gmail.com>
2026-03-18 20:07:11 +0100

    chore(config): init swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 import { env } from "./env";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("API Cocktails Solvro")
+    .setDescription("Recruitment task for Solvro")
+    .setVersion("1.0")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
   await app.listen(env.PORT);
 }
 bootstrap();
