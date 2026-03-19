7ef38db4e41d9f772905f7e81ee630506b90b229
Stanisław Synowiec <stasyn1410@gmail.com>
2026-03-19 17:36:28 +0100

    chore(category): add cocktail category enum

--- src/db/schema/cocktails.ts
+++ src/db/schema/cocktails.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import { integer, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
 import {
   createInsertSchema,
   createSelectSchema,
@@ -6,9 +6,24 @@ import {
 } from "drizzle-zod";
 import type { z } from "zod";
 
+export const categoriesEnum = pgEnum("categories", [
+  "Cocktail",
+  "Ordinary Drink",
+  "Punch / Party Drink",
+  "Shake",
+  "Other / Unknown",
+  "Cocoa",
+  "Shot",
+  "Coffee / Tea",
+  "Homemade Liqueur",
+  "Soft Drink",
+]);
+
 export const cocktails = pgTable("cocktails", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: text().notNull(),
+  category: categoriesEnum().notNull().default("Other / Unknown"),
+  instructions: text().notNull(),
 });
 
 export const cocktailSelectSchema = createSelectSchema(cocktails);
